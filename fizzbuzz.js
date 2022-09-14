ce9532ef27fc91123ce4b10dffa03b11cfcf46f1
johnmartinezc <john.martinez@codeimmersives.com>
2022-09-14 17:30:42 -0400

    -a

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,22 @@
 const prompt = require('prompt-sync')
 ({sigint: true})
 
-let age = Number(prompt('Enter your age:'))
-console.log(age)
+let number = Number(prompt('Enter a number: '))
+console.log(number)
+
+let f = "fizz";
+let b = "buzz";
+for (let num = 1; num <=700 ; num++) {
+    if (num% 3 === 0 && num % 5 ===0){
+    console.log(num + f + b);
+                }
+    else if (num % 5 === 0){
+    console.log(num+b);
+                }
+    else if (num % 3 === 0){
+    console.log(num+f);
+                }
+    else {
+    console.log(num);
+                }
+            }
