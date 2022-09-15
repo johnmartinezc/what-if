8c7f164168a4df89f4de91fb270bdd5d15c88b96
johnmartinezc <john.martinez@codeimmersives.com>
2022-09-15 12:38:03 -0400

    -a

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,18 +2,16 @@ const prompt = require('prompt-sync')
 ({sigint: true})
 
 let number = Number(prompt('Enter a number: '))
-console.log(number)
+console.log("You have entered: "+ number)
 
-let f = "fizz";
-let b = "buzz";
-for (let num = 1; num <=50 ; num++) {
-    if (num% 3 === 0 && num % 5 ===0){
-    console.log(num + f + b);
-                }
-    else if (num % 5 === 0){
-    console.log(num+b);
-                }
-    else if (num % 3 === 0){
-    console.log(num+f);
-                }    
-            }
+
+
+if(number % 15 === 0  ) {
+    console.log(number + " fizzbuzz " )
+}
+else if(number% 3 === 0){
+    console.log(number + " fizz ")
+} else if(number % 5 === 0){
+    console.log(number + " buzz " )
+}  
+
--- group-sizes.js
+++ group-sizes.js
@@ -5,6 +5,18 @@ let numberOfStudents = Number(prompt('Enter number of students: '))
 console.log(numberOfStudents)
 
 
-if(numberOfStudents ){
-    console.log()
+if(numberOfStudents% 15 === 0 ){
+    console.log(numberOfStudents / 2)
 }
+
+
+
+
+if(number % 15 === 0  ) {
+    console.log(number + " fizzbuzz " )
+}
+else if(number% 3 === 0){
+    console.log(number + " fizz ")
+} else if(number % 5 === 0){
+    console.log(number + " buzz " )
+}  
